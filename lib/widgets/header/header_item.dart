bd08e3f2456b2c81668c5490ad88d568a2825dbf
vpsines <sasip6028@gmail.com>
2023-02-13 14:03:44 +0530

    feat: create header_item widget

---
+++ lib/widgets/header/header_item.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HeaderItem extends StatelessWidget {
+  final String name;
+  final VoidCallback onTap;
+  
+
+  const HeaderItem({super.key, required this.name, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: TextButton(
+        onPressed: onTap,
+        child: Text(name,
+        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black,fontSize: 18),),
+      ),
+    );
+  }
+}
