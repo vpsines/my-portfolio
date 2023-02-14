9e27a3206a4f7f1efd134b8d56658357e8a01d53
vpsines <sasip6028@gmail.com>
2023-02-15 03:49:06 +0530

    feat: create intro section

--- lib/widgets/body/intro/intro.dart
+++ lib/widgets/body/intro/intro.dart
@@ -5,34 +5,57 @@ class IntroWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: const [
-        const SizedBox(
-          height: 20,
+        var size = MediaQuery.of(context).size;
+
+    return SizedBox(
+      height: size.height * 0.9,
+      child: Padding(
+        padding:  EdgeInsets.only(top: size.height * 0.1,bottom: 50),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              const SizedBox(
+                height: 50,
+              ),
+              Text(
+                "Hi, my name is",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Vyshnav Sasidharan",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 64,
+                    letterSpacing: 1.5),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                "A developer focused on building cross-platform applications using Flutter.",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w300,
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
         ),
-        Text(
-          "Hi, my name is",
-          style: TextStyle(
-              color: Colors.black, fontWeight: FontWeight.w300, fontSize: 14),
-        ),
-        const SizedBox(
-          height: 5,
-        ),
-        Text(
-          "Vyshnav Sasidharan",
-          style: TextStyle(
-              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 22),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          "A developer focused on building cross-platform applications using Flutter",
-          style: TextStyle(
-              color: Colors.black, fontWeight: FontWeight.w300, fontSize: 14),
-        )
-      ],
+      ),
     );
   }
 }
