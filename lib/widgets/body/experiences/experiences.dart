557cd4e5f670244f9e91764d47c3134f6e9d37c8
vpsines <sasip6028@gmail.com>
2023-02-16 19:45:41 +0530

    feat: add experience widget

--- lib/widgets/body/experiences/experiences.dart
+++ lib/widgets/body/experiences/experiences.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
-class Experiences extends StatelessWidget {
+class Experiences extends StatefulWidget {
   const Experiences({super.key});
+
+  @override
+  State<Experiences> createState() => _ExperiencesState();
+}
+
+class _ExperiencesState extends State<Experiences> {
+  int currentIndex = 0;
+
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +31,8 @@ class Experiences extends StatelessWidget {
               letterSpacing: 1),
         ),
         SizedBox(
-              height: size.height * 0.075,
-            ),
+          height: size.height * 0.075,
+        ),
         Container(
           height: size.height * 0.6,
           child: Row(
@@ -31,17 +44,39 @@ class Experiences extends StatelessWidget {
                   scrollDirection: Axis.vertical,
                   itemCount: 4,
                   itemBuilder: (context, index) {
-                    return Container(
-                      width: size.width * 0.1,
-                      height: size.height * 0.05,
-                      decoration: const BoxDecoration(color: Colors.black12),
-                      child: Center(
-                        child: Text(
-                          "${index + 1}",
-                          style: const TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 14),
+                    return GestureDetector(
+                      onTap: () => onTap(index),
+                      child: Container(
+                        width: size.width * 0.1,
+                        height: size.height * 0.05,
+                        decoration: BoxDecoration(
+                            color: (currentIndex == index)
+                                ? Colors.white
+                                : Colors.black12),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            (index == currentIndex)
+                                ? Container(
+                                    color: Colors.black,
+                                    width: 3,
+                                    height: double.infinity,
+                                  )
+                                : Container(),
+                            Expanded(
+                              child: Center(
+                                child: Text(
+                                  "${index + 1}",
+                                  style: TextStyle(
+                                      color: (currentIndex == index)
+                                          ? Colors.black
+                                          : Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     );
@@ -50,12 +85,12 @@ class Experiences extends StatelessWidget {
               ),
               Expanded(
                   child: Padding(
-                    padding:  EdgeInsets.only(left: size.width * 0.025,top: size.height * 0.01),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
+                padding: EdgeInsets.only(
+                    left: size.width * 0.025, top: size.height * 0.01),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
                     RichText(
-                      
                       text: const TextSpan(
                           text: "Backend Developer ",
                           style: TextStyle(
@@ -74,7 +109,9 @@ class Experiences extends StatelessWidget {
                             )
                           ]),
                     ),
-                    SizedBox(height: size.height * 0.015,),
+                    SizedBox(
+                      height: size.height * 0.015,
+                    ),
                     const Text(
                       "January 2020 - February 2020",
                       style: TextStyle(
@@ -83,9 +120,9 @@ class Experiences extends StatelessWidget {
                           color: Colors.black,
                           letterSpacing: 1),
                     ),
-                ],
-              ),
-                  ))
+                  ],
+                ),
+              ))
             ],
           ),
         )
