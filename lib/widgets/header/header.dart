e1c3434b84ef24c85e5a8f45523f1d84eb468787
vpsines <sasip6028@gmail.com>
2023-03-15 23:02:14 +0530

    feat: add ontap for header items

--- lib/widgets/header/header.dart
+++ lib/widgets/header/header.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:my_portfolio/widgets/header/header_item.dart';
 
 class Header extends StatelessWidget {
-  const Header({super.key});
+  final ValueNotifier<int?> navItemNotifier;
+  final ValueNotifier<bool> isHeaderItemPressed;
+
+  const Header(
+      {super.key,
+      required this.navItemNotifier,
+      required this.isHeaderItemPressed});
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +18,30 @@ class Header extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          HeaderItem(name: "ABOUT", onTap: () {}),
-          HeaderItem(name: "EXPERIENCES", onTap: () {}),
-          HeaderItem(name: "PROJECTS", onTap: () {}),
-          HeaderItem(name: "CONTACT", onTap: () {}),
+          HeaderItem(
+              name: "ABOUT",
+              onTap: () {
+                navItemNotifier.value = 1;
+                isHeaderItemPressed.value = true;
+              }),
+          HeaderItem(
+              name: "EXPERIENCES",
+              onTap: () {
+                navItemNotifier.value = 2;
+                isHeaderItemPressed.value = true;
+              }),
+          HeaderItem(
+              name: "PROJECTS",
+              onTap: () {
+                navItemNotifier.value = 3;
+                isHeaderItemPressed.value = true;
+              }),
+          HeaderItem(
+              name: "CONTACT",
+              onTap: () {
+                navItemNotifier.value = 4;
+                isHeaderItemPressed.value = true;
+              }),
         ],
       ),
     );
