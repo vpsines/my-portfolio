9c4ee636fcb4d089ab285c2041f96b1825491470
vpsines <sasip6028@gmail.com>
2023-03-18 10:35:32 +0530

    feat: add header value notifiers

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/widgets/body/body.dart';
 import 'package:my_portfolio/widgets/header/header.dart';
@@ -11,42 +12,54 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final ValueNotifier<int?> headerNotifier = ValueNotifier(0);
+  final ValueNotifier<bool> isHeaderItemPressed = ValueNotifier(false);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-          preferredSize: const Size(double.infinity, 75),
-          child: AppBar(
-            leadingWidth: 250,
-            leading: Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: const [
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: CircleAvatar(
-                      backgroundColor: Colors.black,
-                      radius: 30,
-                    ),
+        appBar: PreferredSize(
+            preferredSize: const Size(double.infinity, 75),
+            child: AppBar(
+                leadingWidth: 250,
+                leading: Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.black,
+                          radius: 30,
+                        ),
+                      ),
+                      Text("Vyshnav Sasidharan")
+                    ],
                   ),
-                  Text("Vyshnav Sasidharan")
-                ],
-              ),
-            ),
-            title: Header()
-          )),
-          body: Stack(
-            children: [
-              BodyWidget(),
-              Positioned(
+                ),
+                title: Header(
+                  navItemNotifier: headerNotifier,
+                  isHeaderItemPressed: isHeaderItemPressed,
+                ))),
+        body: Stack(
+          children: [
+            ValueListenableBuilder(
+                valueListenable: isHeaderItemPressed,
+                builder: (BuildContext context, bool? value, Widget? child) {
+                  return BodyWidget(
+                    sectionNotifier: headerNotifier,
+                    isHeaderItemPressed: isHeaderItemPressed,
+                  );
+                }),
+            Positioned(
                 left: 0,
                 top: MediaQuery.of(context).size.height * 0.3,
-                child: SocialBar(isFooter: false,)
-              )
-            ],
-          )
-    );
+                child: SocialBar(
+                  isFooter: false,
+                ))
+          ],
+        ));
   }
 }
