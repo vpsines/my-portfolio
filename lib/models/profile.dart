b4046a2ff4c787491d605bd673249de3fb331b1f
vpsines <sasip6028@gmail.com>
2023-03-23 20:08:36 +0530

    refactor: add profile model

---
+++ lib/models/profile.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Profile{
+final String about;
+  final String profileUrl;
+  final List<String> skills;
+
+  Profile(
+      {required this.about,
+      required this.profileUrl,
+      required this.skills});
+
+  Profile.fromDocumentSnapshot(DocumentSnapshot<Map<String,dynamic>> documentSnapshot)
+      : this(
+            about: documentSnapshot.get("about"),
+            profileUrl: documentSnapshot.get("profileUrl"),
+            skills: documentSnapshot.get("skills")
+            );
+}
+
