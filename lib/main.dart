7c2803d7c9f39e0247954a064d14a76f2649a80f
vpsines <sasip6028@gmail.com>
2023-03-27 19:04:56 +0530

    feat: integrate provider notifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:my_portfolio/firebase_options.dart';
+import 'package:my_portfolio/providers/data_provider.dart';
+import 'package:my_portfolio/screens/data_screen.dart';
 import 'package:my_portfolio/screens/home_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -14,10 +21,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home:  HomeScreen(),
+      home: ChangeNotifierProvider<DataProvider>(
+        create: (context) => DataProvider(),
+        child: const HomeScreen(),
+      ),
     );
   }
 }
