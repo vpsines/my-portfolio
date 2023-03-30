007977a4fcf977041fe71ca895cd7011713ff1ce
vpsines <sasip6028@gmail.com>
2023-03-30 10:02:11 +0530

    refactor: add index field

--- lib/models/section.dart
+++ lib/models/section.dart
@@ -3,11 +3,12 @@ import 'package:equatable/equatable.dart';
 class Section extends Equatable{
   final String sectionName;
   final UpdateSource source;
+  final int index;
 
-  Section({required this.sectionName,required this.source});
+  Section({required this.sectionName,required this.source,required this.index});
 
   @override
   List<Object?> get props => [sectionName,source];
 }
 
-enum UpdateSource{scroll,click,addressBar}
+enum UpdateSource{scroll,click,addressBar,none}
